65d14ec937ef0716da1703a4c60fb4bb4a1308f0
Nathanael <nathanael.hollings@gmail.com>
2019-07-01 20:37:53 +1200

    connects client to feathers

--- src/store.js
+++ src/store.js
@@ -1,16 +1,30 @@
-import Vue from 'vue';
-import Vuex from 'vuex';
+import Vue from "vue";
+import Vuex from "vuex";
+import feathersVuex from "feathers-vuex";
+import feathersClient from "./feathers-client";
+
+const { service, auth, FeathersVuex } = feathersVuex(feathersClient, {
+  idField: "_id"
+});
 
 Vue.use(Vuex);
+Vue.use(FeathersVuex);
 
 export default new Vuex.Store({
-  state: {
-
-  },
-  mutations: {
-
-  },
-  actions: {
-
-  },
+  plugins: [
+    service("users", {
+      instanceDefaults: {
+        name: "",
+        username: "",
+        password: "",
+        email: ""
+      }
+    }),
+    auth({
+      userService: "users"
+    })
+  ],
+  state: {},
+  mutations: {},
+  actions: {}
 });
